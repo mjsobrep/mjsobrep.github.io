c2187e8e288ce65e4d8ea624c65995a38bdcdbaf
Sobrepera, Michael Joseph <mjsobrep@live.com>
2015-06-20 22:18:30 -0400

    got the basics running

---
+++ _plugins/stripFile.rb
@@ -0,0 +1,50 @@
+require 'liquid'
+require 'uri'
+
+module Jekyll
+  module StripFile
+    def folder(input)
+      input[0,input.rindex("/")]
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::StripFile)
+
+module Jekyll
+  module StrCmp
+    def compare(input1,input2)
+        input1<=>input2
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::StrCmp)
+
+module Jekyll
+  module PathMatch
+    def pathMatch(candidate,pattern)
+        rawPath = candidate["path"]
+        trimmedPath = rawPath[0,rawPath.rindex("/")]
+        toReturn = nil
+        if((trimmedPath<=>pattern)==0)
+            toReturn= true
+        end
+        return toReturn
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::PathMatch)
+
+
+ 
+# Percent encoding for URI confrming to RFC 3986.
+# Ref: http://tools.ietf.org/html/rfc3986#page-12
+module URLEncode
+  def url_encode(url)
+    return url.gsub(' ','%20')
+  end
+end
+
+Liquid::Template.register_filter(URLEncode)
---
+++ _posts/projects/2010HomeComing/2010-10-22-homecoming.markdown
@@ -0,0 +1,9 @@
+---
+layout: post
+title:  "Homecoming 2010"
+date:   2010-10-22
+categories: projects
+imageDir: 2010Homecoming
+---
+
+During high school I was highly involved in the student government. One of the big events events every year was the homecoming dance. We spent a lot of time and money building really elaborate decorations. In 2010 the theme was "Arabian Nights".
