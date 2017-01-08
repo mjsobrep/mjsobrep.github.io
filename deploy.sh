61c505ba92a99bcca53d6212542ede5c68a8d470
Travis CI <mjsobrep@live.com>
2017-01-08 03:47:17 +0000

    automated commit to publish from travis

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Taken from https://github.com/felixrieseberg/travis-jekyll-git/blob/master/scripts/deploy.sh
+# and https://github.com/steveklabnik/automatically_update_github_pages_with_travis_example
+
+# set to error if fail. Set to error if any unset variables
+set -o errexit -o nounset
+
+
+echo "deploying to master branch from deploy branch"
+cd _site
+
+git init
+git config user.name "Travis CI"
+git config user.email "mjsobrep@live.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/mjsobrep/mjsobrep.github.io.git"
+git fetch upstream
+git reset upstream/master # Make all of our changes off of the master branch
+touch . # make all of the files look fresh
+
+
+git add -A :/
+git commit -m 'automated commit to publish from travis';
+git push -q upstream HEAD:master
+
+exit 0
